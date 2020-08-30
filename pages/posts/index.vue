0abb948b78539a97e10f4118ce0c9d4555095560
Chaloulou75 <c.jeandey@gmail.com>
2020-08-29 22:45:34 -0300

    text

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="px-4 py-4 mx-auto font-semibold">
-    <h1 class="font-bold text-center text-gray-100">Posts du Blog de Carpinto</h1>
+    <h1 class="font-bold text-center text-gray-100 truncate">Cette page est juste pour dire que je peux faire un Blog</h1>
       <ul class="px-12">
         <li class="px-4 py-4 mx-auto " v-for="(post, index) in posts" :key="index">
-          <NuxtLink :to="`/posts/${post.slug}`"class="text-gray-400">{{ post.title }} by {{ post.author }}</NuxtLink>
+          <NuxtLink :to="`/posts/${post.slug}`" class="text-gray-400">{{ post.title }} by {{ post.author }}</NuxtLink>
         </li>
       </ul>
   </div>
