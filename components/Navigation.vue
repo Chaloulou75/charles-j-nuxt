fa6c54d84207ab4a53dcfb2317020ee7951cd02d
Chaloulou75 <c.jeandey@gmail.com>
2020-09-08 11:40:16 -0300

    nav mobile

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,22 +1,46 @@
 <template>
-  <nav class="flex justify-between w-full px-8 py-8 text-white bg-gray-800 border-green-700 border-t-14">
+  <div>
+    <nav class="flex justify-between w-full px-8 py-8 text-white bg-gray-800 border-green-700 border-t-14">
 
       <NuxtLink to="/" class="font-semibold uppercase">Charles Jeandey</NuxtLink>
 
-      <ul class="flex justify-around space-x-6 font-semibold">
+      <button
+        class="outline-none mobile-hamburger lg:hidden focus:outline-none hover:text-green-700"
+        @click="show = !show"
+      >
+        <svg v-if="!show" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        <svg v-if="show" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+      </button>
 
-        <li><NuxtLink to="/posts">Blog</NuxtLink></li>
-        <nuxt-link
-          v-for="locale in availableLocales"
-          :key="locale.code"
-          :to="switchLocalePath(locale.code)">{{ locale.name }}</nuxt-link>
-
-      </ul>
-  </nav>
+      <div class="hidden desktop-menu lg:block">
+        <ul class="flex justify-around space-x-6 font-semibold">
+          <li><NuxtLink to="/posts">Blog</NuxtLink></li>
+          <nuxt-link
+            v-for="locale in availableLocales"
+            :key="locale.code"
+            :to="switchLocalePath(locale.code)">{{ locale.name }}</nuxt-link>
+        </ul>
+      </div>
+    </nav>
+    <transition name="fade" class="mobile-menu lg:hidden">
+        <ul class="flex flex-col items-end px-8 space-y-2 text-sm font-semibold" v-if="show">
+          <li><NuxtLink to="/posts" >Blog</NuxtLink></li>
+          <nuxt-link
+            v-for="locale in availableLocales"
+            :key="locale.code"
+            :to="switchLocalePath(locale.code)">{{ locale.name }}</nuxt-link>
+        </ul>
+    </transition>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      show: false
+    }
+  },
   computed: {
     availableLocales () {
       return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
@@ -24,3 +48,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
