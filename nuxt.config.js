f1f3f5b2a6649d5aa3f0f6f27039c845326977c9
Chaloulou75 <c.jeandey@gmail.com>
2020-10-20 15:21:25 -0300

    router

--- nuxt.config.js
+++ nuxt.config.js
@@ -170,20 +170,33 @@ export default {
   },
 
   router: {
-    scrollBehavior(to) {
-      if (to.hash) {
-        const element = document.querySelector(to.hash);
-        if (element) {
-          window.scrollTo({
-            top: element.offsetTop,
-            behavior: 'smooth'
-          });
+    router: {
+      scrollBehavior: async (to, from, savedPosition) => {
+        if (savedPosition) {
+          return savedPosition
         }
+
+        const findEl = async (hash, x) => {
+          return document.querySelector(hash) ||
+            new Promise((resolve, reject) => {
+              if (x > 50) {
+                return resolve()
+              }
+              setTimeout(() => { resolve(findEl(hash, ++x || 1)) }, 200)
+            })
+        }
+
+        if (to.hash) {
+          let el = await findEl(to.hash)
+          if ('scrollBehavior' in document.documentElement.style) {
+            return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
+          } else {
+            return window.scrollTo(0, el.offsetTop)
+          }
+        }
+
+        return { x: 0, y: 0 }
       }
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
     }
   },
 }
