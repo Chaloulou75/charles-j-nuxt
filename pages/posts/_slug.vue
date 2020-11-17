2f8c4f108ec5268b36b4023109d623940dea7655
Chaloulou75 <c.jeandey@gmail.com>
2020-11-17 11:57:07 -0300

    404bis

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,4 +1,8 @@
 <template>
+<div>
+  <div id="back"></div>
+  <div id="front"></div>
+  <Navigation />
   <div class="px-2 mx-auto lg:px-20">
     <div class="px-4 py-4 mx-auto font-semibold text-center text-gray-100">{{ postInMarkdown.title}}</div>
     <div class="px-4 py-4 mx-auto text-center text-gray-300">by: {{ postInMarkdown.author}}</div>
@@ -6,6 +10,7 @@
     <nuxt-content :document="postInMarkdown" />
 
   </div>
+</div>
 </template>
 
 <script>
