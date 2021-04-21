6cfeb5e46e3b71be78850a009446b5599a73c822
Chaloulou75 <c.jeandey@gmail.com>
2021-04-21 18:27:57 -0300

    change recaptcha for honeypot

--- components/Form.vue
+++ components/Form.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center max-w-md px-3 py-10 mx-auto mt-3 mb-8 bg-gray-100 border border-gray-500 rounded-lg shadow-xl dark:border-gray-300 lg:px-4 lg:max-w-xl dark:bg-bluefonce">
-    <form name="contact" action="POST" data-netlify="true" data-netlify-recaptcha="true">
+    <form name="contact" action="POST" netlify-honeypot="bot-field" data-netlify="true">
     <div>
       <label class="text-sm font-bold text-gray-800 dark:text-gray-200" for="username">{{ $t('nometprenom')}}:
       <input class="w-full p-3 mt-2 text-gray-900 bg-gray-200 rounded-lg shadow-md focus:outline-none focus:shadow-outline focus:ring-4 dark:focus:ring-blue-300 focus:ring-bluefonce ring-offset-2 focus:ring-opacity-30" type="text" name="username" id="username"></label>
@@ -18,7 +18,9 @@
       <textarea name="message" id="message" class="w-full h-32 p-3 mt-2 text-gray-900 bg-gray-200 rounded-lg shadow-md focus:outline-none focus:shadow-outline focus:ring-4 dark:focus:ring-blue-300 focus:ring-bluefonce ring-offset-2 focus:ring-opacity-30"></textarea></label>
     </div>
 
-    <div data-netlify-recaptcha="true" class="mt-8"></div>
+    <div class="hidden">
+      <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+    </div>
 
     <div class="mt-8">
       <button type="submit"
