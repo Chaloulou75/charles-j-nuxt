f49a7f9bf88335a91e47267ed73243e1f803ac07
Chaloulou75 <c.jeandey@gmail.com>
2021-04-29 17:11:36 -0300

    lazy component2

--- components/Contact.vue
+++ components/Contact.vue
@@ -8,25 +8,25 @@
     <div class="grid grid-flow-col grid-cols-4 gap-4 py-4 mx-auto">
       <div class="flex items-center justify-center h-full">
         <Mailto mail="c.jeandey@gmail.com" class="text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100">
-            <LogosGmail class="w-10 h-10" fill="#EA4335" />
+            <LazyLogosGmail class="w-10 h-10" fill="#EA4335" />
             <span class="sr-only">email</span>
         </Mailto>
       </div>
       <div class="flex items-center justify-center h-full">
         <a href="https://twitter.com/CarlitosBuck" target="_blank" rel=”noreferrer” class="text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100">
-            <LogosTwitter class="w-10 h-10" fill="#1DA1F2" />
+            <LazyLogosTwitter class="w-10 h-10" fill="#1DA1F2" />
             <span class="sr-only">Twitter</span>
           </a>
       </div>
       <div class="flex items-center justify-center h-full">
         <a href="https://www.linkedin.com/in/charles-jeandey" target="_blank" rel=”noreferrer” class="text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100">
-          <LogosLinkedin class="w-10 h-10" fill="#0A66C2" />
+          <LazyLogosLinkedin class="w-10 h-10" fill="#0A66C2" />
           <span class="sr-only">linkedin</span>
         </a>
       </div>
       <div class="flex items-center justify-center h-full">
         <a href="https://github.com/Chaloulou75" target="_blank" rel=”noreferrer” class="text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100">
-            <LogosGithub class="w-10 h-10 text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100" fill="currentColor" />
+            <LazyLogosGithub class="w-10 h-10 text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100" fill="currentColor" />
             <span class="sr-only">Github</span>
           </a>
       </div>
